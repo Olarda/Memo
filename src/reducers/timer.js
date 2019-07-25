2481189045d2e9ec0f5f320bc1ec81a5750a7061
Olarda <dariafurzikova@yandex.ru>
2019-07-25 22:15:36 +0300

    fix: delete console log

--- src/reducers/timer.js
+++ src/reducers/timer.js
@@ -1,24 +1,20 @@
 
 const initialState = {
-    timer: 5,
+    timer: 81,
 
 };
 
 export default function timer(state = initialState, action) {
     switch (action.type) {
     case 'SET_START':
-        console.log('start');
         return { ...state, timer };
 
     case 'GAME_STATE':
-        console.log(action.payload, 'payload');
-        // checkedCard.push(action.payload);
 
         return { ...state, gameState: action.payload };
 
     case 'SET_END':
 
-        console.log(action.payload, 'payload');
         return { ...state, end: action.payload };
 
     default:
